7f0b5a5192b4eef83c4113513b605b042e3f8a49
joenapper <joe.napper@hotmail.co.uk>
2021-04-25 19:06:44 +0100

    Created sign up page using Bootstrap and added sign up functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { Signup } from "./components/Signup";
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "./contexts/AuthContext";
+
 function App() {
   return (
-    <div className="App">
-      <h1>heello</h1>
-    </div>
+    <AuthProvider>
+      <div className="App">
+        <Container
+          className="d-flex align-items-center justify-content-center"
+          style={{ minHeight: "100vh" }}
+        >
+          <div className="w-100" style={{ maxWidth: "400px" }}>
+            <Signup />
+          </div>
+        </Container>
+      </div>
+    </AuthProvider>
   );
 }
 
